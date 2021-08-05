b28214696d59f7e76468cc3e61731b839f730afa
ChangNam Yoo <ycn0320@gmail.com>
2021-08-05 12:12:28 +0900

    샘플 코드와 샘플 코드를 위한 더미 코드

---
+++ SampleCode/SampleCode/CachedDataClass.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace SampleCode
+{
+    // 장비 아이템 외에 능력치를 올려주는 버프에서 사용할 인터페이스 추가
+    public interface IExtraStatData
+    {
+        StatType StatType { get; }
+        int StatValue { get; }
+        bool CheckActiveCondition(DungeonData data);
+    }
+
+    // 펫 버프 캐싱에 사용할 부모 클래스
+    // 차후 스킬 버프가 추가 될 경우 조건 체크는 동일하기 때문에
+    // CheckActiveCondition는 부모 클래스에 구현
+    public class CachedPetBuffDataBase
+    {
+        public int Index { get; set; }
+
+        public bool CheckActiveCondition(DungeonData data)
+        {
+            if (data == null)
+                return false;
+
+            var petBuffData = TableDataManger.Instance.PetBuffDataContainer.GetData(Index);
+            if (petBuffData == null)
+                return false;
+
+            var isActive = false;
+
+            switch (petBuffData.ConditionType)
+            {
+                case PetBuffConditionType.None:
+                    {
+                        isActive = true;
+                    }
+                    break;
+                case PetBuffConditionType.BattleType:   
+                    {
+                        // petBuffData.ConditionValue값이 현재 던전의 BattleType과 같으면 버프 적용
+                        isActive = IsActiveBattleType(petBuffData.ConditionValue, data.BattleType);
+                    }
+                    break;
+                case PetBuffConditionType.Dungeon:
+                    {
+                        // petBuffData.ConditionValue값이 현재 던전의 DungeonIndex와 같으면 버프 적용
+                        isActive = IsActiveCampaign(petBuffData.ConditionValue, data.DungeonIndex);
+                    }
+                    break;
+            }
+
+            return isActive;
+        }
+
+        // 활성 조건 체크
+        #region active condition Check
+        bool IsActiveBattleType(string[] conditionValue, BattleType currentBattleType)
+        {
+            var isActive = false;
+            // ...
+            return isActive;
+        }
+
+        bool IsActiveCampaign(string[] conditionValue, int currentDungeonIndex)
+        {
+            var isActive = false;
+            // ...
+            return isActive;
+        }
+        #endregion
+    }
+
+    // CachedPetStatBuffData 을 상속받지 못하게 sealed 사용
+    public sealed class CachedPetStatBuffData : CachedPetBuffDataBase, IExtraStatData
+    {
+        public StatType StatType { get; set; }
+        public int StatValue { get; set; }
+
+        StatType IExtraStatData.StatType { get { return StatType; } }
+        int IExtraStatData.StatValue { get { return StatValue; } }
+
+        bool IExtraStatData.CheckActiveCondition(DungeonData data)
+        {
+            var result = CheckActiveCondition(data);
+            return result;
+        }
+    }
+}
---
+++ SampleCode/SampleCode/Dummy.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// 더미 클래스
+namespace SampleCode
+{
+    public enum StatType
+    {
+        None = 0,
+    }
+
+    public enum BattleType
+    {
+        None = 0,
+    }
+
+    public enum PetBuffConditionType
+    {
+        None = 0,
+        BattleType,
+        Dungeon,
+    }
+
+    public class DungeonData
+    {        
+        public int DungeonIndex { get; set; }
+
+        public BattleType BattleType { get; set; }
+    }
+
+    public class PetBuffData
+    {
+        public PetBuffConditionType ConditionType { get; set; }
+        public string[] ConditionValue { get; set; }
+    }
+
+    public class PetBuffDataContainer
+    {
+        public PetBuffData GetData(int index)
+        {
+            return null;
+        }
+    }
+
+    public class TableDataManger
+    {
+        public static TableDataManger Instance
+        {
+            get { return GetInstance(); }
+        }
+
+        public PetBuffDataContainer PetBuffDataContainer { get; private set; }
+
+        static TableDataManger _instance;
+
+        static TableDataManger GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new TableDataManger();
+            }
+            return _instance;
+        }
+    }
+
+}
